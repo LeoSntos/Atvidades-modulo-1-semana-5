05587a61730afe0d1ffd23003a3fc7402e15da73
LeoSntos <leobrognoli38@gmail.com>
2024-03-26 18:50:37 -0300

    exercício 3

--- js/script.js
+++ js/script.js
@@ -1,25 +1,66 @@
-function consultar() {
-    const produto = document.getElementById('informe').value.toLocaleLowerCase()
-    switch (produto) {
-        case "banana":
-        case "#001":
-            document.getElementById('preco').innerText = 'O valor do produto é R$ 4,90Kg'
-            return;
 
-        case "mamao":
-        case "#002":
-            document.getElementById('preco').innerText = 'O valor do produto é R$ 7,99Kg'
-            return;
-        
-        case "morango":
-        case "#003":
-            document.getElementById('preco').innerText = 'O valor do produto é R$ 12,50Kg'
-            return;
-        
-        case "abacate":
-        case "#003":
-            document.getElementById('preco').innerText = 'O valor do produto é R$ 14,99Kg'
-            return;
-        
+// consultar o preco
+let produtos = [
+    {
+        "nome": "banana",
+        "preco": "R$ 4,90Kg",
+        "codigo": "#001"
+    },
+    {
+        "nome": "mamao",
+        "preco": "R$ 8,90Kg",
+        "codigo": "#002"
+    },
+    {
+        "nome": "pessego",
+        "preco": "R$ 6,90Kg",
+        "codigo": "#003"
+    },
+    {
+        "nome": "morango",
+        "preco": "R$ 10,90Kg",
+        "codigo": "#004"
+    },
+    {
+        "nome": "abacate",
+        "preco": "R$ 12,90Kg",
+        "codigo": "#005"
     }
+]
+
+
+function consultar() {
+    let input = document.getElementById('informe').value.toLocaleLowerCase()
+    for (let i = 0; i < produtos.length; i++) {
+        if (produtos[i].nome.toLocaleLowerCase() === input || produtos[i].codigo === input) {
+            alert(produtos[i].preco)
+        }
+    }
+}
+
+// comprar 
+
+
+let carrinho = [] 
+
+function comprar() {   
+    let input = document.getElementById('informe').value.toLocaleLowerCase()
+    let achouProduto = null
+
+    for (let i = 0; i < produtos.length; i++) {
+        if (produtos[i].nome.toLocaleLowerCase() === input || produtos[i].codigo === input) {
+            achouProduto = produtos[i]
+        }
+    }
+
+    if (achouProduto) {
+        carrinho.unshift({
+            nome: achouProduto.nome,
+            preco: achouProduto.preco
+        })
+    } else {
+        alert('produto nao encontrado')
+    }
+
+    console.log(carrinho)
 }
